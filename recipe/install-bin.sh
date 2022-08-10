0aa874bd6d5e9d69216d1cca480dd31f8f2080a7
duncan.macleod <duncan.macleod@ligo.org>
2022-08-10 10:26:17 +0100

    abstract complicated make expression as macro
    hopefully simplifies the install scripts

--- recipe/install-bin.sh
+++ recipe/install-bin.sh
@@ -2,17 +2,20 @@
 
 set -ex
 
+# macros
+_make="make -j ${CPU_COUNT} V=1 VERBOSE=1"
+
 # install from python build directory
 _pybuilddir="_build${PY_VER}"
 cd ${_pybuilddir}
 
 # test binaries
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
-	make -j ${CPU_COUNT} V=1 VERBOSE=1 check -C bin
+	${_make} check -C bin
 fi
 
 # install binaries
-make -j ${CPU_COUNT} V=1 VERBOSE=1 install -C bin
+${_make} install -C bin
 
 # install system configuration files
-make -j ${CPU_COUNT} V=1 VERBOSE=1 install-sysconfDATA
+${_make} install-sysconfDATA
--- recipe/install-lib.sh
+++ recipe/install-lib.sh
@@ -2,13 +2,16 @@
 
 set -ex
 
+# macros
+_make="make -j ${CPU_COUNT} V=1 VERBOSE=1"
+
 cd _build
 
 # install library and headers
-make -j ${CPU_COUNT} V=1 VERBOSE=1 -C lib install HDF5_LIBS="-L${PREFIX} -lhdf5 -lhdf5_hl"
+${_make} -C lib install HDF5_LIBS="-L${PREFIX} -lhdf5 -lhdf5_hl"
 
 # install SWIG binding definitions and headers
-make -j ${CPU_COUNT} V=1 VERBOSE=1 -C swig install-data
+${_make} -C swig install-data
 
 # install pkg-config
-make -j ${CPU_COUNT} V=1 VERBOSE=1 install-pkgconfigDATA
+${_make} install-pkgconfigDATA
