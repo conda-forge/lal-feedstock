376215a99989dc7cf407300376802e88f7dc3fdf
duncan.macleod <duncan.macleod@ligo.org>
2022-11-24 16:34:44 +0000

    move common build options to common.sh script
    which is sourced by each build script, should reduce mismatch errors

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,42 +1,20 @@
 #!/bin/bash
 
-set -e
+set -ex
 
-# macros
-_make="make -j ${CPU_COUNT} V=1 VERBOSE=1"
+# get common options
+. ${RECIPE_DIR}/common.sh
 
 # use out-of-tree build
 mkdir -pv _build
 cd _build
 
-# replace package name in debug-prefix-map with source name
-export CFLAGS=$(
-   echo ${CFLAGS:-} |
-   sed -E 's|'\/usr\/local\/src\/conda\/${PKG_NAME}'|/usr/local/src/conda/lal|g'
-)
-
-# select FFT implementation
-if [[ "${fft_impl}" == "mkl" ]]; then
-    FFT_CONFIG_ARGS="--disable-static --enable-intelfft"
-else
-    FFT_CONFIG_ARGS=""
-fi
-
-# only link libraries we actually use
-export GSL_LIBS="-L${PREFIX}/lib -lgsl"
-export HDF5_LIBS="-L${PREFIX}/lib -lhdf5 -lhdf5_hl"
-
 # configure
 ${SRC_DIR}/configure \
-	--disable-doxygen \
-	--disable-gcc-flags \
-	--disable-python \
-	--disable-swig-octave \
-	--disable-swig-python \
-	--enable-swig-iface \
-	--prefix="${PREFIX}" \
-	--with-hdf5=yes \
-	${FFT_CONFIG_ARGS} \
+  ${CONFIGURE_ARGS} \
+  --disable-python \
+  --disable-swig-python \
+  --enable-swig-iface \
 ;
 
 # build
